fc9e5573764adbf1e6f8116c347f8ecd1ae00c24
Sriharsha Guduguntla <sguduguntla@Sriharshas-MacBook-Pro.local>
2019-12-08 16:21:36 -0800

    Editing version number

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, 'README.md')) as f:
 
 setup(
     name="bldg_point_clustering",
-    version="0.0.1",
+    version="0.0.2",
     author="Sriharsha Guduguntla",
     author_email="sguduguntla@berkeley.edu",
     description="A Python 3.5+ wrapper for clustering building point labels using KMeans, DBScan, and Agglomerative clustering",
